fb7c48942c2317b7d4f589fa3859f12301cd6f95
Jeffreylwang94 <jefflwang94@gmail.com>
2024-04-25 15:16:35 -0700

    added full game logic

--- javascript-RPS.js
+++ javascript-RPS.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     let rand_num = Math.floor(Math.random()*3+1);
     let output = "rock";
@@ -31,4 +32,33 @@ function randomTester(iterations){
     for (let result in dictionary){
         console.log(result+": "+String(dictionary[result]));
     }
+}
+
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice,computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice == computerChoice) {
+            console.log("Tie! You both chose "+humanChoice);
+        } else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+                   (humanChoice == "scissors" && computerChoice == "paper") ||
+                   (humanChoice == "paper" && computerChoice == "rock")){
+            console.log("You win! "+humanChoice+" beats "+computerChoice+".");
+            humanScore++;
+        } else {
+            console.log("You lose! "+computerChoice +" beats "+humanChoice +".");
+            computerScore++;
+        }
+        
+    }
+
+    for (let round = 1; round <= 5; round++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    console.log("Human Score: "+humanScore);
+    console.log("Computer Score: " + computerScore);
 }
